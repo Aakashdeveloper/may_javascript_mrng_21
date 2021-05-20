94a373678e693f4fa61ecc7e28b2b884c4b3a254
aakashdeveloper <ahanda205@gmail.com>
2021-05-20 08:42:09 +0530

    condition

---
+++ 3operatoers.js
@@ -0,0 +1,69 @@
+= //assignment
+== // compare the data
+=== // compare data and datatype
+
+var a = 20
+var b = "20"
+
+var a = 20
+var b = "20"
+
+a == b   > compare the data
+true
+a === b  > compare data and datatype
+false
+
+var a = 20
+var b = 20
+a == b 
+true
+a === b
+true
+
+var a = 10
+var b = 5
+
+a !== b
+true
+a >= b
+true
+a <= b
+false
+a =< b
+
+all compare operater will always return boolean response 
+
+//negation
+var a = true
+!a 
+var a = true
+!a 
+false
+
+var b = false
+undefined
+!a
+false
+!b
+true
+
+
+var a = 1
+var b = 0
+var c = -1
+
+!a
+false
+
+!b
+true
+
+!c 
+false
+
+var a = "Hii"
+!a
+false
+
+truthy > any number except 0 weather +ve or -ve / string/ true  
+falsy > 0,false, null , undefined
---
+++ 4confirm.js
@@ -0,0 +1,52 @@
+console.log() >>>> for checking output and debug 
+alert() >> for notification
+prompt() >> to take input from user
+confirm() >>> to check weather yes or no 
+
+alert("Form submitted")
+undefined
+alert("See is full")
+undefined
+
+confirm("Do you want to leave")
+true
+
+confirm("Do you want to leave")
+false
+
+var out = confirm("Do you want to leave")
+out
+true
+
+var out = confirm("Do you want to leave")
+out
+false
+
+prompt("Enter your age")
+"10"
+var age = prompt("Enter your age")
+age
+"10"
+
+var age = prompt("Enter your name")
+age
+"aakash"
+
+var a = prompt("Enter first number");
+var b = prompt("Enter second number");
+alert(a+b)
+
+var a = prompt("Enter first number");
+var b = prompt("Enter second number");
+alert(parseFloat(a)+parseFloat(b))
+
+var a = prompt("Enter first number");"6"
+var b = prompt("Enter second number");"5"
+alert(Number(a)+Number(b))
+11
+
+var a = prompt("Enter first number"); "6"
+var b = prompt("Enter second number"); "5"
+
+Number("65")
+65
